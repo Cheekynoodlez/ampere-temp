19c3a5e10f1c8b545e64c4594b9a22a2a68c426b
cheekynoodlez <106006654+Cheekynoodlez@users.noreply.github.com>
2025-12-12 18:17:19 -0800

    Update expressions.py
    transpose test

--- expressions.py
+++ expressions.py
@@ -35,9 +35,14 @@ for frame_index in range(0, num_frames):
     gif.seek(frame_index)
     # must copy the frame out of the gif, since thumbnail() modifies the image in-place
     frame = gif.copy()
-    frame.thumbnail((matrix.width, matrix.height), Image.Resampling.LANCZOS)
+
+    combined = Image.new(RGB, (matrix.width * 2, matrix.height))
+    combined.paste(frame, (0,0))
+    combined.paste(frame.transpose(Image.FLIP_LEFT_RIGHT, (matrix.width, 0))
+    
+    combined.thumbnail((matrix.width, matrix.height), Image.Resampling.LANCZOS)
     canvas = matrix.CreateFrameCanvas()
-    canvas.SetImage(frame.convert("RGB"))
+    canvas.SetImage(combined.convert("RGB"))
     canvases.append(canvas)
 # Close the gif file to save memory now that we have copied out all of the frames
 gif.close()
